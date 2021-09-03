279f200c40be7de42ee805490c7d74a7244ee614
Daisykm <56872925+Daisykm@users.noreply.github.com>
2021-09-03 09:48:22 +0200

    project update

--- Lærlingenes Huset/Assets/Scripts/ExpandCollapse.cs
+++ Lærlingenes Huset/Assets/Scripts/ExpandCollapse.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Cinemachine;
 
 public class ExpandCollapse : MonoBehaviour
 {
@@ -13,6 +14,7 @@ public class ExpandCollapse : MonoBehaviour
   public Vector3 originalPosition;
   public Vector3 newPosition;
   public GameObject gameObject;
+  [SerializeField] public CinemachineVirtualCamera _CMvcam1, _CMvcam2Window, _CMvcam3Wall;
 
 
   private void Start()
@@ -34,16 +36,36 @@ public class ExpandCollapse : MonoBehaviour
       //houseExpanded.SetActive(true);
       IsExpanded = true;
       gameObject.transform.position = newPosition;
+
+     
     }
-    
+
+
+
     else if (IsExpanded == true)
     {
       //houseCollapsed.SetActive(true);
      // houseExpanded.SetActive(false);
       IsExpanded = false;
       gameObject.transform.position = originalPosition;
+      
+      
     }
   
+    if (Input.GetMouseButtonDown(0) && IsExpanded == true)
+    {
+      RaycastHit rayCastHit;
+      Ray rayCast = Camera.main.ScreenPointToRay(Input.mousePosition);
+      if (Physics.Raycast(rayCast, out rayCastHit))
+      {
+
+        if (rayCastHit.collider.CompareTag("window"))
+        {
+          _CMvcam2Window.Priority = _CMvcam1.Priority + 1;
+
+        }
+      }
+    }
    
   }
  
